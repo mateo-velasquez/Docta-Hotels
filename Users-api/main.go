4d8188eeed69a32cf3c94e4b007a020b682318d1
mateo-velasquez <mateovelasquezborda04@gmail.com>
2026-06-29 01:36:22 -0300

    feat: config to the main

--- Users-api/main.go
+++ Users-api/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"users-api/src/app"
+	// "users-api/src/db"
+	// "users-api/src/utils"
+)
+
+func main() {
+
+	// db.StartDbEngine()
+	// utils.InitCache()
+	app.StartRoute()
+}
