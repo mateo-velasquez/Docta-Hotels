51038c1489e55d9f7110af519050b284e08db839
mateo-velasquez <mateovelasquezborda04@gmail.com>
2026-06-30 02:54:28 -0300

    feat: add the config to use environment variables

--- Users-api/config/config_users.go
+++ Users-api/config/config_users.go
@@ -1,1 +1,40 @@
 package config
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+const (
+	CacheDuration = 30 * time.Second
+	JWTDuration   = 24 * time.Hour
+)
+
+var (
+	MySQLHost     string
+	MySQLPort     string
+	MySQLDatabase string
+	MySQLUsername string
+	MySQLPassword string
+	MemcachedHost string
+	MemcachedPort string
+	JWTKey        string
+)
+
+func init() {
+	if err := godotenv.Load("config/.env"); err != nil {
+		fmt.Println("warning: config/.env not found, using environment variables")
+	}
+
+	MySQLHost = os.Getenv("MYSQL_HOST")
+	MySQLPort = os.Getenv("MYSQL_PORT")
+	MySQLDatabase = os.Getenv("MYSQL_DATABASE")
+	MySQLUsername = os.Getenv("MYSQL_USERNAME")
+	MySQLPassword = os.Getenv("MYSQL_PASSWORD")
+	MemcachedHost = os.Getenv("MEMCACHED_HOST")
+	MemcachedPort = os.Getenv("MEMCACHED_PORT")
+	JWTKey = os.Getenv("JWT_KEY")
+}
