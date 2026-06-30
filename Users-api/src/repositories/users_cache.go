17e389ef9e60ee9456b192c7a97a7806a2230321
mateo-velasquez <mateovelasquezborda04@gmail.com>
2026-06-30 02:49:06 -0300

    feat: add the repositories, but mocks still needs to be created

---
+++ Users-api/src/repositories/users_cache.go
@@ -0,0 +1,86 @@
+package repositories_users
+
+import (
+	"fmt"
+	"time"
+	dao "users-api/src/dao"
+
+	"github.com/karlseguin/ccache"
+)
+
+type CacheConfig struct {
+	TTL          time.Duration
+	MaxSize      int64
+	ItemsToPrune uint32
+}
+
+const (
+	keyByID    = "user_id:%d"
+	keyByEmail = "user_email:%s"
+)
+
+type Cache struct {
+	client *ccache.Cache
+	ttl    time.Duration
+}
+
+func NewCache(config CacheConfig) Cache {
+	client := ccache.New(ccache.Configure().
+		MaxSize(config.MaxSize).
+		ItemsToPrune(config.ItemsToPrune))
+	return Cache{
+		client: client,
+		ttl:    config.TTL,
+	}
+}
+
+func (repository Cache) GetUserById(id int64) (dao.User, error) {
+	key := fmt.Sprintf(keyByID, id)
+	item := repository.client.Get(key)
+	fmt.Println(key)
+	if item == nil {
+		return dao.User{}, fmt.Errorf("not found item with key %s", key)
+	}
+	if item.Expired() {
+		return dao.User{}, fmt.Errorf("item with key %s is expired", key)
+	}
+	userDAO, ok := item.Value().(dao.User)
+	if !ok {
+		return dao.User{}, fmt.Errorf("error converting item with key %s", key)
+	}
+	return userDAO, nil
+}
+
+func (repository Cache) GetUserByEmail(Email string) (dao.User, error) {
+	fmt.Println("Buscando", Email)
+
+	// Use username as cache key
+	userKey := fmt.Sprintf(keyByEmail, Email)
+
+	// Try to get from cache
+	item := repository.client.Get(userKey)
+	if item != nil && !item.Expired() {
+		// Return cached value
+		user, ok := item.Value().(dao.User)
+		if !ok {
+			return dao.User{}, fmt.Errorf("failed to cast cached value to user")
+		}
+		return user, nil
+	}
+
+	// If not found, return cache miss error
+	return dao.User{}, fmt.Errorf("cache miss for email %s", Email)
+}
+
+func (repository Cache) CreateUser(user dao.User) (int64, error) {
+	keyByID := fmt.Sprintf(keyByID, user.Id)
+	keyByEmail := fmt.Sprintf(keyByEmail, user.Email)
+	fmt.Println("Guardando", keyByID)
+	fmt.Println("Guardando", keyByEmail)
+	fmt.Println("saving with duration", repository.ttl)
+	repository.client.Set(keyByID, user, repository.ttl)
+	repository.client.Set(keyByEmail, user, repository.ttl)
+	return int64(user.Id), nil
+}
+
+//agregar login
