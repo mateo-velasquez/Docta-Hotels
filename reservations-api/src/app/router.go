a82f4a431a0f00a5cc4d906f720d6a3f161c50c5
mateo-velasquez <mateovelasquezborda04@gmail.com>
2026-07-02 03:53:13 -0300

    feat: Basic config

--- reservations-api/src/app/router.go
+++ reservations-api/src/app/router.go
@@ -1,1 +1,33 @@
 package app
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+var router *gin.Engine
+
+func init() {
+	router = gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+}
+
+func StartRoute() {
+	initDependencies()
+	mapUrls()
+
+	log.Info("Starting server")
+	if err := router.Run(":8086"); err != nil {
+		log.Panicf("Error running application: %v", err)
+	}
+}
--- reservations-api/src/app/url_mappings.go
+++ reservations-api/src/app/url_mappings.go
@@ -1,1 +1,14 @@
 package app
+
+import log "github.com/sirupsen/logrus"
+
+func mapUrls() {
+	router.POST("/reservations", serviceController.InsertReservation)
+	router.GET("/reservations/:id", serviceController.GetReservationById)
+	router.GET("/reservations", serviceController.GetReservations)
+	router.GET("/reservations/user/:id", serviceController.GetReservationsByUser)
+	router.PUT("/reservations/:id", serviceController.UpdateReservation)
+	router.DELETE("/reservations/:id", serviceController.DeleteReservation)
+
+	log.Info("Finishing mappings configurations")
+}
