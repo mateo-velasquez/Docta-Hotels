9f800f9b23b89491ecf2a56097c8eb9ca3c49a3b
mateo-velasquez <mateovelasquezborda04@gmail.com>
2026-07-02 03:56:47 -0300

    feat: add the reservations logic

--- reservations-api/src/domain/reservations_domain.go
+++ reservations-api/src/domain/reservations_domain.go
@@ -1,1 +1,11 @@
 package domain
+
+// DTO para Reservaciones
+type Reservation struct {
+	Id        int     `json:"id"`
+	StartDate string  `json:"start_date"`
+	EndDate   string  `json:"end_date"`
+	UserId    int     `json:"user_id"`
+	HotelId   string  `json:"hotel_id"`
+	Amount    float64 `json:"amount"`
+}
