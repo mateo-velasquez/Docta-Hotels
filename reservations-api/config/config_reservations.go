959dd6d1358ccdccc0d871fc6d53f77fd5f49a4b
mateo-velasquez <mateovelasquezborda04@gmail.com>
2026-07-02 03:48:22 -0300

    feat: add environment variables

--- reservations-api/config/config_reservations.go
+++ reservations-api/config/config_reservations.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	MySQLHost     string
+	MySQLPort     string
+	MySQLDatabase string
+	MySQLUsername string
+	MySQLPassword string
+)
+
+func init() {
+	if err := godotenv.Load("config/.env"); err != nil {
+		fmt.Println("warning: config/.env not found, using environment variables")
+	}
+
+	MySQLHost = os.Getenv("MYSQL_HOST")
+	MySQLPort = os.Getenv("MYSQL_PORT")
+	MySQLDatabase = os.Getenv("MYSQL_DATABASE")
+	MySQLUsername = os.Getenv("MYSQL_USERNAME")
+	MySQLPassword = os.Getenv("MYSQL_PASSWORD")
+}
