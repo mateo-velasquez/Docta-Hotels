f9c35fca8cb3c26e93e43c6b40975d4cf76b5bbd
mateo-velasquez <mateovelasquezborda04@gmail.com>
2026-07-02 03:59:35 -0300

    feat: add the error manage

---
+++ reservations-api/src/utils/errors.go
@@ -0,0 +1,59 @@
+package utils
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type CauseList []interface{}
+
+type ApiError interface {
+	Message() string
+	Code() string
+	Status() int
+	Cause() CauseList
+	Error() string
+}
+
+type apiErr struct {
+	ErrorMessage string    `json:"message"`
+	ErrorCode    string    `json:"error"`
+	ErrorStatus  int       `json:"status"`
+	ErrorCause   CauseList `json:"cause"`
+}
+
+func (e apiErr) Code() string {
+	return e.ErrorCode
+}
+
+func (e apiErr) Error() string {
+	return fmt.Sprintf("Message: %s;Error Code: %s;Status: %d;Cause: %v", e.ErrorMessage, e.ErrorCode, e.ErrorStatus, e.ErrorCause)
+}
+
+func (e apiErr) Status() int {
+	return e.ErrorStatus
+}
+
+func (e apiErr) Cause() CauseList {
+	return e.ErrorCause
+}
+
+func (e apiErr) Message() string {
+	return e.ErrorMessage
+}
+
+func NewNotFoundApiError(message string) ApiError {
+	return apiErr{message, "not_found", http.StatusNotFound, CauseList{}}
+}
+
+func NewBadRequestApiError(message string) ApiError {
+	return apiErr{message, "bad_request", http.StatusBadRequest, CauseList{}}
+}
+
+func NewInternalServerApiError(message string, err error) ApiError {
+	cause := CauseList{}
+	if err != nil {
+		cause = append(cause, err.Error())
+	}
+	return apiErr{message, "internal_server_error", http.StatusInternalServerError, cause}
+}
